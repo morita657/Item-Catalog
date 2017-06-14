d112dafa4ae9144014346706942831761b38a457
Takahiro Morita <taro@Takahiros-MacBook-Air.local>
2017-06-14 21:54:37 +0700

    Delete user_id from login_session

--- finalProject.py
+++ finalProject.py
@@ -219,6 +219,7 @@ def gdisconnect():
         del login_session['username']
         del login_session['email']
         del login_session['picture']
+        del login_session['user_id']
 
         response = make_response(json.dumps('Successfully disconnected.'
                                  ), 200)
