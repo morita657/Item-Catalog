350a26fb5d1b7890824141e807b35e18e9ab82d9
Takahiro Morita <taro@Takahiros-MacBook-Air.local>
2017-07-01 21:26:07 +0700

    Fix minor bug

--- finalProject.py
+++ finalProject.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from flask import Flask
 from project import app
 
-app = Flask(__name__)
+# app = Flask(__name__)
 app.secret_key = 'super'
+# app.config['SESSION_TYPE'] = 'filesystem'
 app.debug = True
+# app.run(host='0.0.0.0', port=5000)
