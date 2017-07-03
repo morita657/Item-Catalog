d529106f9bd0812bdd8ede86e44cd12a58917caa
Takahiro Morita <taro@Takahiros-MacBook-Air.local>
2017-07-03 22:38:00 +0700

    Add configuration vatiable

--- finalProject.py
+++ finalProject.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import os
 from flask import Flask
 from project import app
 
 # app = Flask(__name__)
-app.secret_key = 'super'
-app.config['SESSION_TYPE'] = 'filesystem'
-app.debug = True
+# app = Flask(__name__)
+# app.secret_key = 'super'
+# app.config['SESSION_TYPE'] = 'filesystem'
+port = int(os.environ.get('PORT', 5000))
+server_address = ('', port)
+# httpd = http.server.HTTPServer(server_address, Shortener)
+app.config.from_object('config')
+# app.debug = True
 # app.run(host='0.0.0.0', port=5000)
