fc27aff7bb3808ad830a3a6994113abd2cf1d03a
Takahiro Morita <taro@Takahiros-MacBook-Air.local>
2017-07-04 18:30:33 +0700

    Minor change

--- finalProject.py
+++ finalProject.py
@@ -8,9 +8,10 @@ from project import app
 # app = Flask(__name__)
 # app.secret_key = 'super'
 # app.config['SESSION_TYPE'] = 'filesystem'
+app.debug = True
 port = int(os.environ.get('PORT', 5000))
 server_address = ('', port)
 # httpd = http.server.HTTPServer(server_address, Shortener)
 app.config.from_object('config')
-# app.debug = True
+
 # app.run(host='0.0.0.0', port=5000)
