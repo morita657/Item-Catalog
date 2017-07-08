58566ffaef872446f2990b7f4bf30a248ab442f8
Takahiro Morita <morita657@gmail.com>
2017-07-08 07:47:49 +0700

    Reincrement secret key

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 # enable CSRF
 WTF_CSRF_ENABLED = True
-
+secret_key_generator = os.urandom(24)
 # secret key for authentication
-SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "secret")
+SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", secret_key_generator)
