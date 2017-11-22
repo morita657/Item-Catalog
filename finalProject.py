c5acf77febeb053362a54c2d81f1334764d38167
Takahiro Morita <morita657@gmail.com>
2017-11-22 22:53:02 +0700

    Comment out app.run

--- finalProject.py
+++ finalProject.py
@@ -8,4 +8,4 @@ app.debug = True
 port = int(os.environ.get('PORT', 5000))
 server_address = ('', port)
 app.config.from_object('config')
-app.run('0.0.0.0', port=5000)
+# app.run('0.0.0.0', port=5000)
